a37b92201b7a24b59a3c236a1bbfa86c41a89086
tossenxD <thorbjorn.bb@gmail.com>
2024-03-04 03:23:26 +0100

    ten addition versions for each kernel

--- cuda/kernels.cu.h
+++ cuda/kernels.cu.h
@@ -54,29 +54,25 @@ public:
     }
 };
 
+
+
 /*******************************/
 /*** Big Integer Addition v1 ***/
 /*******************************/
 
-// Each block adds one instance of big integers and each thread adds one unit
 template<class Base, uint32_t m>
-__global__ void baddKer1(typename Base::uint_t* as, typename Base::uint_t* bs, typename Base::uint_t* rs) {
+__device__ inline typename Base::uint_t
+baddKer1Run(typename Base::uint_t a, typename Base::uint_t b, typename Base::carry_t* shmem) {
     using uint_t  = typename Base::uint_t;
     using carry_t = typename Base::carry_t;
-    __shared__ carry_t shmem[m];
-
-    // 1. fetch units from global memory
-    uint64_t gid = blockIdx.x * m + threadIdx.x;
-    uint_t  a = as[gid];
-    uint_t  b = bs[gid];
-
-    // 2. compute result and carry
-    uint_t  r = a + b;
+    
+    // 1. compute result and carry
+    uint_t r = a + b;
     carry_t c = ((carry_t) (r < a)) | (((carry_t) (r == Base::HIGHEST)) << 1);
     shmem[threadIdx.x] = c;
     __syncthreads();
 
-    // 3. propagate carries by exclusive scan
+    // 2. propagate carries by exclusive scan
     c = scanIncBlock< CarryProp<Base> >(shmem, threadIdx.x);
     __syncthreads();
     shmem[threadIdx.x] = CarryProp<Base>::identity();
@@ -86,32 +82,69 @@ __global__ void baddKer1(typename Base::uint_t* as, typename Base::uint_t* bs, t
     __syncthreads();
     c = shmem[threadIdx.x];
 
-    // 4. write result to global memory
-    rs[gid] = r + (c & 1);    
+    // 3. add carries to result and return
+    return r + (c & 1);
 }
+
+// Each block adds one instance of big integers and each thread adds one unit
+template<class Base, uint32_t m>
+__global__ void
+baddKer1(typename Base::uint_t* as, typename Base::uint_t* bs, typename Base::uint_t* rs) {
+    using uint_t  = typename Base::uint_t;
+    using carry_t = typename Base::carry_t;
+
+    // 1. fetch units from global memory
+    uint64_t gid = blockIdx.x * m + threadIdx.x;
+    uint_t a = as[gid];
+    uint_t b = bs[gid];
+
+    // 2. compute results with carry propagation
+    __shared__ carry_t shmem[m];
+    uint_t r = baddKer1Run<Base,m>(a, b, shmem);
+
+    // 3. write result to global memory
+    rs[gid] = r;    
+}
+
+// Computes ten additions as above - for benchmarking
+template<class Base, uint32_t m>
+__global__ void
+baddKer1Ten(typename Base::uint_t* as, typename Base::uint_t* bs, typename Base::uint_t* rs) {
+    using uint_t  = typename Base::uint_t;
+    using carry_t = typename Base::carry_t;
+
+    // 1. fetch units from global memory
+    uint64_t gid = blockIdx.x * m + threadIdx.x;
+    uint_t a = as[gid];
+    uint_t b = bs[gid];
+
+    // 2. compute results with carry propagation tenfold
+    __shared__ carry_t shmem[m];
+    uint_t r = b;
+    for(int i=0; i<10; i++) {
+        r = baddKer1Run<Base,m>(a, r, shmem);
+        __syncthreads();
+    }
+
+    // 3. write result to global memory
+    rs[gid] = r;    
+}
+
+
 
 /*******************************/
 /*** Big Integer Addition v2 ***/
 /*******************************/
 
-// Each block adds one instance of big integers and each thread adds multiple units
 template<class Base, uint32_t m, uint32_t q>
-__global__ void baddKer2(typename Base::uint_t* as, typename Base::uint_t* bs, typename Base::uint_t* rs) {
+__device__ void
+baddKer2Run(typename Base::uint_t* ass, typename Base::uint_t* bss,
+            typename Base::uint_t* rss, typename Base::carry_t* shmem) {
     using uint_t  = typename Base::uint_t;
     using carry_t = typename Base::carry_t;
-    __shared__ carry_t shmem[m];
-    uint_t  ass[q];
-    uint_t  bss[q];
-    uint_t  rss[q];
+    
+    // 1. compute result, carry and thread-level scan
     carry_t css[q];
-
-    // 1. fetch units from global memory
-    cpGlb2Reg<uint_t,m,q,1>(as, ass);
-    __syncthreads();
-    cpGlb2Reg<uint_t,m,q,1>(bs, bss);
-    __syncthreads();
-
-    // 2. compute result, carry and thread-level scan
     carry_t acc = CarryProp<Base>::identity();
     for(int i=0; i<q; i++) {
         rss[i] = ass[i] + bss[i];
@@ -121,7 +154,7 @@ __global__ void baddKer2(typename Base::uint_t* as, typename Base::uint_t* bs, t
     shmem[threadIdx.x] = acc;
     __syncthreads();
 
-    // 3. propagate carries and write them to shared memory
+    // 2. propagate carries and write them to shared memory
     acc = scanIncBlock< CarryProp<Base> >(shmem, threadIdx.x);
     __syncthreads();
     shmem[threadIdx.x] = CarryProp<Base>::identity();
@@ -129,37 +162,78 @@ __global__ void baddKer2(typename Base::uint_t* as, typename Base::uint_t* bs, t
         shmem[threadIdx.x+1] = acc;
     __syncthreads();
 
-    // 4. write result to global memory
+    // 3. add carries to results
     acc = shmem[threadIdx.x];
     for(int i=0; i<q; i++) {
         rss[i] += acc & 1;
         acc = CarryProp<Base>::apply(acc, css[i]);
     }
+}
+
+// Each block adds one instance of big integers and each thread adds multiple units
+template<class Base, uint32_t m, uint32_t q>
+__global__ void
+baddKer2(typename Base::uint_t* as, typename Base::uint_t* bs, typename Base::uint_t* rs) {
+    using uint_t  = typename Base::uint_t;
+    using carry_t = typename Base::carry_t;
+
+    // 1. fetch units from global memory
+    uint_t ass[q];
+    uint_t bss[q];
+    cpGlb2Reg<uint_t,m,q,1>(as, ass);
+    cpGlb2Reg<uint_t,m,q,1>(bs, bss);
+    __syncthreads();
+
+    // 2. compute results with carry propagation
+    uint_t rss[q];
+    __shared__ carry_t shmem[m];
+    baddKer2Run<Base,m,q>(ass, bss, rss, shmem);
+
+    // 3. write result to global memory
     cpReg2Glb<uint_t,m,q,1>(rss, rs);
 }
+
+// Computes ten additions as above - for benchmarking
+template<class Base, uint32_t m, uint32_t q>
+__global__ void
+baddKer2Ten(typename Base::uint_t* as, typename Base::uint_t* bs, typename Base::uint_t* rs) {
+    using uint_t  = typename Base::uint_t;
+    using carry_t = typename Base::carry_t;
+
+    // 1. fetch units from global memory
+    uint_t ass[q];
+    uint_t bss[q];
+    cpGlb2Reg<uint_t,m,q,1>(as, ass);
+    cpGlb2Reg<uint_t,m,q,1>(bs, bss);
+    __syncthreads();
+
+    // 2. compute results with carry propagation tenfold
+    uint_t* rss = bss;
+    __shared__ carry_t shmem[m];
+    for(int i=0; i<10; i++) {
+        baddKer2Run<Base,m,q>(ass, rss, rss, shmem);
+        __syncthreads();
+    }
+
+    // 3. write result to global memory
+    cpReg2Glb<uint_t,m,q,1>(rss, rs);
+}
+
+
 
 /*******************************/
 /*** Big Integer Addition v3 ***/
 /*******************************/
 
-// Each block adds multiple instances of big integers and each thread adds multiple units
 template<class Base, uint32_t m, uint32_t q, uint32_t ipb>
-__global__ void baddKer3(typename Base::uint_t* as, typename Base::uint_t* bs, typename Base::uint_t* rs) {
-    using uint_t = typename Base::uint_t;
+__device__ void
+baddKer3Run(typename Base::uint_t* ass, typename Base::uint_t* bss,
+            typename Base::uint_t* rss, typename Base::carry_t* shmem) {
+    using uint_t  = typename Base::uint_t;
     using carry_t = typename Base::carry_t;
-    __shared__ carry_t shmem[m*ipb];
-    uint_t ass[q];
-    uint_t bss[q];
-    uint_t rss[q];
-    uint_t css[q];
 
-    // 1. copy from global memory to registers
-    cpGlb2Reg<uint_t,m,q,ipb>(as, ass);
-    __syncthreads();
-    cpGlb2Reg<uint_t,m,q,ipb>(bs, bss);
-    __syncthreads();
-    
-    // 2. compute result, carry and thread-level scan
+    // 1. compute result, carry and thread-level scan
+    uint_t css[q];
     carry_t acc = SegCarryProp<Base>::identity();
     for(int i=0; i<q; i++) {
         rss[i] = ass[i] + bss[i];
@@ -170,7 +244,7 @@ __global__ void baddKer3(typename Base::uint_t* as, typename Base::uint_t* bs, t
     shmem[threadIdx.x] = acc;
     __syncthreads();
 
-    // 3. propagate carries and write them to shared memory
+    // 2. propagate carries and write them to shared memory
     acc = scanIncBlock< SegCarryProp<Base> >(shmem, threadIdx.x);
     __syncthreads();
     shmem[threadIdx.x] = acc;
@@ -178,11 +252,59 @@ __global__ void baddKer3(typename Base::uint_t* as, typename Base::uint_t* bs, t
     acc = (threadIdx.x % (m/q) == 0) ? SegCarryProp<Base>::identity() : shmem[threadIdx.x-1];
     __syncthreads();
 
-    // 4. write results to global memory
+    // 3. add carries to results
     for(int i=0; i<q; i++) {
         rss[i] += (acc & 1);
         acc = SegCarryProp<Base>::apply(acc, css[i]);
     }
+}
+
+// Each block adds multiple instances of big integers and each thread adds multiple units
+template<class Base, uint32_t m, uint32_t q, uint32_t ipb>
+__global__ void
+baddKer3(typename Base::uint_t* as, typename Base::uint_t* bs, typename Base::uint_t* rs) {
+    using uint_t  = typename Base::uint_t;
+    using carry_t = typename Base::carry_t;
+
+    // 1. copy from global memory to registers
+    uint_t ass[q];
+    uint_t bss[q];
+    cpGlb2Reg<uint_t,m,q,ipb>(as, ass);
+    cpGlb2Reg<uint_t,m,q,ipb>(bs, bss);
+    __syncthreads();
+
+    // 2. compute results with carry propagation
+    uint_t rss[q];
+    __shared__ carry_t shmem[m*ipb];
+    baddKer3Run<Base,m,q,ipb>(ass, bss, rss, shmem);
+
+    // 4. write results to global memory
+    cpReg2Glb<uint_t,m,q,ipb>(rss, rs);
+}
+
+// Computes ten additions as above - for benchmarking
+template<class Base, uint32_t m, uint32_t q, uint32_t ipb>
+__global__ void
+baddKer3Ten(typename Base::uint_t* as, typename Base::uint_t* bs, typename Base::uint_t* rs) {
+    using uint_t  = typename Base::uint_t;
+    using carry_t = typename Base::carry_t;
+
+    // 1. copy from global memory to registers
+    uint_t ass[q];
+    uint_t bss[q];
+    cpGlb2Reg<uint_t,m,q,ipb>(as, ass);
+    cpGlb2Reg<uint_t,m,q,ipb>(bs, bss);
+    __syncthreads();
+
+    // 2. compute results with carry propagation
+    uint_t* rss = bss;
+    __shared__ carry_t shmem[m*ipb];
+    for(int i=0; i<10; i++) {
+        baddKer3Run<Base,m,q,ipb>(ass, rss, rss, shmem);
+        __syncthreads();
+    }
+
+    // 4. write results to global memory
     cpReg2Glb<uint_t,m,q,ipb>(rss, rs);
 }
 
--- cuda/main.cu
+++ cuda/main.cu
@@ -11,8 +11,10 @@ using namespace std;
 /*** Big-Integer Addition ***/
 /****************************/
 
-template<class Base, int m>  // m is the size of the big word in Base::uint_t units
-void gpuAdd (int num_instances, typename Base::uint_t* h_as,
+// wrapper that invokes and times the big int addition GPU kernels;
+// m is the size in Base::uint_t units and v is the kernel version.
+template<class Base, uint32_t m, uint32_t v>
+void gpuAdd (uint32_t num_instances, typename Base::uint_t* h_as,
              typename Base::uint_t* h_bs, typename Base::uint_t* h_rs) {
 
     assert((Base::bits >= 32) && (Base::bits % 32 == 0));
@@ -33,29 +35,43 @@ void gpuAdd (int num_instances, typename Base::uint_t* h_as,
     cudaMemcpy(d_bs, h_bs, mem_size_nums, cudaMemcpyHostToDevice);
 
     // 3. kernel dimensions
-    const uint32_t q = 4;
+    const uint32_t q = (v > 1) ? 4 : 1;
     assert(m%q == 0 && m >= q && m/q <= 1024);
-    const uint32_t ipb = (128 + m/q - 1) / (m/q); // ceil(128/(m/q))
+    const uint32_t ipb = (v > 2) ? (128 + m/q - 1) / (m/q) : 1; // ceil(128/(m/q))
     dim3 block(ipb*(m/q), 1, 1);
     dim3 grid (num_instances/ipb, 1, 1);
-    
-#if DEBUG
+    #if DEBUG
     printf("\n[debug] ipb: %d, num_instances: %d, q: %d, m: %d\n", ipb, num_instances, q, m);
-#endif
+    #endif
 
-    // 4. dry run
-    baddKer3<Base,m,q,ipb><<< grid, block >>>(d_as, d_bs, d_rs);
-    cudaDeviceSynchronize();
-    gpuAssert( cudaPeekAtLastError() );
-
-    // 5. timing instrumentation
+    // 4. one addition
     {
+        // dry run
+        #if v == 1
+        baddKer1<Base,m><<< grid, block >>>(d_as, d_bs, d_rs);
+        #elif v == 2
+        baddKer2<Base,m,q><<< grid, block >>>(d_as, d_bs, d_rs);
+        #else
+        baddKer3<Base,m,q,ipb><<< grid, block >>>(d_as, d_bs, d_rs);
+        #endif
+    
+        cudaDeviceSynchronize();
+        gpuAssert( cudaPeekAtLastError() );
+
+        // timing instrumentation
         uint64_t elapsed;
         struct timeval t_start, t_end, t_diff;
         gettimeofday(&t_start, NULL); 
 
-        for(int i=0; i<GPU_RUNS_ADD; i++)
+        for(int i=0; i<GPU_RUNS_ADD; i++) {
+            #if v == 1
+            baddKer1<Base,m><<< grid, block >>>(d_as, d_bs, d_rs);
+            #elif v == 2
+            baddKer2<Base,m,q><<< grid, block >>>(d_as, d_bs, d_rs);
+            #else
             baddKer3<Base,m,q,ipb><<< grid, block >>>(d_as, d_bs, d_rs);
+            #endif
+        }
 
         cudaDeviceSynchronize();
 
@@ -63,25 +79,75 @@ void gpuAdd (int num_instances, typename Base::uint_t* h_as,
         timeval_subtract(&t_diff, &t_end, &t_start);
         elapsed = (t_diff.tv_sec*1e6+t_diff.tv_usec) / GPU_RUNS_ADD;
 
+        // print results
         gpuAssert( cudaPeekAtLastError() );
 
         double runtime_microsecs = elapsed; 
         double bytes_accesses = 3.0 * num_instances * m * sizeof(uint_t);  
         double gigabytes = bytes_accesses / (runtime_microsecs * 1000);
 
-        printf( "Our Addition of %d-bit Big-Numbers (base u%d) runs %d instances in: \
-                 %lu microsecs, GB/sec: %.2f, Mil-Instances/sec: %.2f\n"
-              , m*Base::bits, Base::bits, num_instances, elapsed, gigabytes, num_instances/runtime_microsecs);
+        printf("One v%d addition  of %d-bit big integers (base u%d) runs %d instances \
+in:\t%lu microsecs, GB/sec: %.2f, Mil-Instances/sec: %.2f\n", v, m*Base::bits, Base::bits,
+               num_instances, elapsed, gigabytes, num_instances/runtime_microsecs);
+        
+        cudaMemcpy(h_rs, d_rs, mem_size_nums, cudaMemcpyDeviceToHost);
+        cudaDeviceSynchronize();
+    }
+    
+    // 5. ten additions
+    {
+        // dry run
+        #if v == 1
+        baddKer1Ten<Base,m><<< grid, block >>>(d_as, d_bs, d_rs);
+        #elif v == 2
+        baddKer2Ten<Base,m,q><<< grid, block >>>(d_as, d_bs, d_rs);
+        #else
+        baddKer3Ten<Base,m,q,ipb><<< grid, block >>>(d_as, d_bs, d_rs);
+        #endif
+    
+        cudaDeviceSynchronize();
+        gpuAssert( cudaPeekAtLastError() );
+
+        // timing instrumentation
+        uint64_t elapsed;
+        struct timeval t_start, t_end, t_diff;
+        gettimeofday(&t_start, NULL); 
+
+        for(int i=0; i<GPU_RUNS_ADD; i++) {
+            #if v == 1
+            baddKer1Ten<Base,m><<< grid, block >>>(d_as, d_bs, d_rs);
+            #elif v == 2
+            baddKer2Ten<Base,m,q><<< grid, block >>>(d_as, d_bs, d_rs);
+            #else
+            baddKer3Ten<Base,m,q,ipb><<< grid, block >>>(d_as, d_bs, d_rs);
+            #endif
+        }
+
+        cudaDeviceSynchronize();
+
+        gettimeofday(&t_end, NULL);
+        timeval_subtract(&t_diff, &t_end, &t_start);
+        elapsed = (t_diff.tv_sec*1e6+t_diff.tv_usec) / GPU_RUNS_ADD;
+
+        // print results
+        gpuAssert( cudaPeekAtLastError() );
+
+        double runtime_microsecs = elapsed; 
+        double bytes_accesses = 3.0 * num_instances * m * sizeof(uint_t);  
+        double gigabytes = bytes_accesses / (runtime_microsecs * 1000);
+
+        printf("Ten v%d additions of %d-bit big integers (base u%d) runs %d instances \
+in:\t%lu microsecs, GB/sec: %.2f, Mil-Instances/sec: %.2f\n", v, m*Base::bits, Base::bits,
+               num_instances, elapsed, gigabytes, num_instances/runtime_microsecs);
     }
 
     // 6. cleanup
-    cudaMemcpy(h_rs, d_rs, mem_size_nums, cudaMemcpyDeviceToHost);
-    cudaDeviceSynchronize();
     cudaFree(d_as);
     cudaFree(d_bs);
     cudaFree(d_rs);
 }
 
+// performs big int addition using gmp
 template<int m>
 void gmpAdd(int num_instances, uint32_t* as, uint32_t* bs, uint32_t* rs) {
     uint32_t* it_as = as;
@@ -94,37 +160,48 @@ void gmpAdd(int num_instances, uint32_t* as, uint32_t* bs, uint32_t* rs) {
     }
 }
 
-template<class Base, int m> // m is the size of the big word in u32 units
+// wrapper that benchmarks (and possible validates) the big int addition kernels;
+// m is the size of the big word in u32 units.
+template<class Base, int m>
 void testAddition(int num_instances, uint64_t* h_as_64, uint64_t* h_bs_64,
                   uint64_t* h_rs_gmp_64, uint64_t* h_rs_our_64, uint32_t with_validation) {
     
     assert((Base::bits >= 32) && (Base::bits % 32 == 0));
-    
+    printf("\n");
+
     using uint_t = typename Base::uint_t;
     uint_t *h_as = (uint_t*) h_as_64;
     uint_t *h_bs = (uint_t*) h_bs_64;
     uint_t *h_rs_our = (uint_t*) h_rs_our_64;
     uint32_t *h_rs_gmp_32 = (uint32_t*) h_rs_gmp_64;
 
-    gpuAdd<Base, m/(Base::bits/32)>(num_instances, h_as, h_bs, h_rs_our);
-    
-    if(with_validation) {
+    if(with_validation)
         gmpAdd<m>(num_instances, (uint32_t*)h_as, (uint32_t*)h_bs, h_rs_gmp_32);
+
+    gpuAdd<Base,m/(Base::bits/32),1>(num_instances, h_as, h_bs, h_rs_our);
+    if(with_validation)
         validateExact(h_rs_gmp_32, (uint32_t*)h_rs_our, num_instances*m);
-    }
+
+    gpuAdd<Base,m/(Base::bits/32),2>(num_instances, h_as, h_bs, h_rs_our);
+    if(with_validation)
+        validateExact(h_rs_gmp_32, (uint32_t*)h_rs_our, num_instances*m);
+
+    gpuAdd<Base,m/(Base::bits/32),3>(num_instances, h_as, h_bs, h_rs_our);
+    if(with_validation)
+        validateExact(h_rs_gmp_32, (uint32_t*)h_rs_our, num_instances*m);
 }
 
 /*****************************************/
 /*** Main program that runs test suits ***/
 /*****************************************/
- 
+
+// runs the big int addition kernel benchmarks and tests.
 template<typename Base>
 void runAdditions(uint64_t total_work) {
     uint64_t *h_as, *h_bs, *h_rs_gmp, *h_rs_our;
     mkRandArrays<32,32>( total_work/32, &h_as, &h_bs, &h_rs_gmp, &h_rs_our );
 
     testAddition<Base, 2048>( total_work/2048, h_as, h_bs, h_rs_gmp, h_rs_our, WITH_VALIDATION );
-#if 1
     testAddition<Base, 1024>( total_work/1024, h_as, h_bs, h_rs_gmp, h_rs_our, WITH_VALIDATION );
     testAddition<Base, 512> ( total_work/512,  h_as, h_bs, h_rs_gmp, h_rs_our, WITH_VALIDATION );
     testAddition<Base, 256> ( total_work/256,  h_as, h_bs, h_rs_gmp, h_rs_our, WITH_VALIDATION );
@@ -132,8 +209,7 @@ void runAdditions(uint64_t total_work) {
     testAddition<Base, 64>  ( total_work/64,   h_as, h_bs, h_rs_gmp, h_rs_our, WITH_VALIDATION );
     testAddition<Base, 32>  ( total_work/32,   h_as, h_bs, h_rs_gmp, h_rs_our, WITH_VALIDATION );
     testAddition<Base, 16>  ( total_work/16,   h_as, h_bs, h_rs_gmp, h_rs_our, WITH_VALIDATION );
-#endif
-    
+
     free(h_as);
     free(h_bs);
     free(h_rs_gmp);
